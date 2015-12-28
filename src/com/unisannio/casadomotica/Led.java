430871c7d262c270d3e8fe02d04b5af051015f2a
Anna Vacca <annavacca21@gmail.com>
2015-12-28 12:58:42 +0100

    update classe led

--- src/com/unisannio/casadomotica/Led.java
+++ src/com/unisannio/casadomotica/Led.java
@@ -1,34 +1,41 @@
 package com.unisannio.casadomotica;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 public class Led {
-	int stato;
-	int stanza;
 
-		public Led(int stanza) {
-				
-			this.stanza = stanza;
-		}
-
-		public int getStato() {
-			return stato;
-		}
-
-		public void setStato(int stato) {
-			this.stato = stato;
-		}
-
-		public int getStanza() {
-			   return stanza;
-		}
-
-		public void setStanza(int stanza) {
-			this.stanza = stanza;
-		}
 		
-		public void accendiLed (int stato){  //metodo accediLed
+		
+		public static void accendiLed (int stanza,int stato){  //metodo accediLed
 			
-			
+			if(stanza==1){
+				if (stato==1){
+					
+					commandArduino("http://arduino201.homepc.it/?leds1ON");
+					
+					
+				}
+				else{
+					commandArduino("http://arduino201.homepc.it/?leds1OFF");
+				
+					
+				}
 			
 		}
 		
 }
+		public static void commandArduino(String url){
+		 	try {
+		 		 
+		 		 HttpClient httpclient = new DefaultHttpClient();
+		 		httpclient.execute(new HttpGet(url));	
+		   		
+		   	
+			    			    
+			  } catch (Exception e) {
+			  }
+		}
+}
+
