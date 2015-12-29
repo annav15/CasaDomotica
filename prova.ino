165bc612f4501a81547426517630cf58467d34b1
nardonemairo <nardonemario@live.it>
2015-12-29 11:27:11 +0100

    Inserimento sensore di temperatura
    Ho inserito un sensore di temperatura, compilando non ci sono problemi vedi se nel monitor seriale è presente il valore del sensore di temperatura.
    Non ho inserito la casella di testo nella pagina web perché mi dava problemi di compilazione, provo a risolvere tale problema

--- prova.ino
+++ prova.ino
@@ -1,8 +1,13 @@
-//////////////////////      LIBRERIE      ////////////////////////
+///////////////////////      LIBRERIE      ////////////////////////
 
 #include <String.h>
 #include<SPI.h>
 #include <Ethernet.h>
+
+#include <OneWire.h>    // librerie per il funzionamento dei sensori di temperatura 
+#include <DallasTemperature.h>
+
+#define temperaturaint A2  // dichiarazione dell'ingresso analogico A2 di arduino 
 
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; // mac address dell'arduino
@@ -13,16 +18,21 @@ EthernetServer server(80); // porta di comunicazione del server
 
 int luce1 = 2 ;   //pin di connessione led di arduino
 
-int luce2 = 3; 
+int luce2 = 3;
 
 String readString ;
 
 boolean LEDON1 = false ; // dichiarazione di una variabile boolean per verificare l'accenzione o meno del led
 
-boolean LEDON2 = false ; 
+boolean LEDON2 = false ;
+
+float temp1;      // variabile temperatura da rilevare
 
 
+OneWire ourWireint(temperaturaint);
+DallasTemperature sensoreint(&ourWireint);
 
+double valtemperaturaint;
 
 
 
@@ -30,15 +40,28 @@ void setup() {
 
 
   Ethernet.begin(mac, ip, gateway, subnet); // inizializzazione dela comunicazione Ethernet
+
+
   pinMode(luce1, OUTPUT);   // dichiarazione del pin 2 come uscita
-  
+
   pinMode(luce2, OUTPUT);
 
   Serial.begin(9600);  // inizializzazione della porta seriale di comunicazione
-
+  sensoreint.begin();
 }
 
 void loop() {
+
+  sensoreint.requestTemperatures();
+
+  valtemperaturaint = sensoreint.getTempCByIndex(0);
+
+  Serial.println("LA TEMPERATURA INTERNA E' = ");
+
+  Serial.println(valtemperaturaint);
+
+  Serial.println("*C ");
+
 
 
   /////       INIZIO CLIENT     /////
@@ -85,34 +108,34 @@ void loop() {
           if (LEDON2 == false) {
             digitalWrite(luce2, LOW);
           }
-          
-            client.println("HTTP/1.1 200 OK.....");
-            client.println("Content-Type: text/html");
-            client.println();
-            // inizializzo pagina (da togliere se uso ajax)
-            client.print("<html><head><title>ARDUINO Controllo Led via WEB</title></head><body>");
-            //iniza il body
-            client.println("<div style='width:1280px; height:720px;'>");
-            client.println("<h1>STATO SENSORE</h1><hr />");
+
+          client.println("HTTP/1.1 200 OK.....");
+          client.println("Content-Type: text/html");
+          client.println();
+          // inizializzo pagina (da togliere se uso ajax)
+          client.print("<html><head><title>ARDUINO Controllo Led via WEB</title></head><body>");
+          //iniza il body
+          client.println("<div style='width:1280px; height:720px;'>");
+          client.println("<h1>STATO SENSORE</h1><hr />");
 
 
-            client.println("<p>Stato LED 1 = ");
-            client.println("<input type=text id=stato1 value=");
-            client.println(LEDON1);
-            client.println(">");
-            client.println("  <br /></p>");
+          client.println("<p>Stato LED 1 = ");
+          client.println("<input type=text id=stato1 value=");
+          client.println(LEDON1);
+          client.println(">");
+          client.println("  <br /></p>");
 
 
-            client.println("<p>Stato LED 2 = ");
-            client.println("<input type=text id=stato1 value=");
-            client.println(LEDON2);
-            client.println(">");
-            client.println("  <br /></p>");
+          client.println("<p>Stato LED 2 = ");
+          client.println("<input type=text id=stato1 value=");
+          client.println(LEDON2);
+          client.println(">");
+          client.println("  <br /></p>");
 
-            client.println("</body></html>");
-            readString = "";
-            client.stop();
-          
+          client.println("</body></html>");
+          readString = "";
+          client.stop();
+
         } // Fine lettura Stream
       } //Fine attivazione
     } //Fine connessione
